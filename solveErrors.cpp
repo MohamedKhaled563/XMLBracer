324880a04cffbdea0f60a10b3d883227aea8c44f
DESKTOP-OMBAEMC\g.version <mohamedtarek251999@gmail.com>
2021-07-15 21:04:24 +0200

    solve errors

--- solveErrors.cpp
+++ solveErrors.cpp
@@ -2,10 +2,38 @@
 
 void MainWindow::on_Solve_Errors_clicked()
 {
+    correctMistakes();      //corrected lines stored in lines vector
+    ui->modifiedXml->clear();
+    std::string line;
+    QTextCharFormat format;
+    QTextCursor cursor( ui->modifiedXml->textCursor() );
+       int j =0;
+       for (unsigned int i=1;i<linesTexts.size()+1;i++)
+       {line=linesTexts[i-1];
+           if(i == mistakes[j])
+           {
+               format.setFontWeight( QFont::TypeWriter );
+               format.setForeground( QBrush( QColor(Qt::darkGreen) ) );
+               cursor.setCharFormat( format );
+               cursor.insertText(QString::fromStdString(line));
+               cursor.insertText("\n");
+               j++;
+           }
+           else
+           {
+               format.setFontWeight( QFont::TypeWriter );
+               format.setForeground( QBrush( QColor(Qt::black) ) );
+               cursor.setCharFormat( format );
+               cursor.insertText(QString::fromStdString(line));
+               cursor.insertText("\n");
+           }
+       }
+
+    /*
     // Clear modifiedXML plain text
     ui->modifiedXml->clear();
     QMessageBox message;
-    string messageText = to_string(solvingLines.size())+" mistakes corrected.\nThe red lines will be deleted";
+    string messageText = to_string(solvingLines.size())+" mistakes corrected.\nThe Green lines should be deleted";
     QString QMessageText =QString::fromStdString(messageText);
     message.setText(QMessageText);
     message.exec();
@@ -36,4 +64,5 @@ void MainWindow::on_Solve_Errors_clicked()
         }
         if(cursor.PreviousCharacter != '\n'){cursor.insertText("\n");}
     }
+    */
 }
